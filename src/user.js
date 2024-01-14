6308e039acbf1610e5fa4485653817e8635afd5b
kyaukyuai <y.kakui@gmail.com>
2024-01-14 21:47:35 +0900

    Add files via your-dev-team

--- src/user.js
+++ src/user.js
@@ -1,35 +1,33 @@
-    const express = require('express');
+const express = require('express');
 const router = express.Router();
 
 const User = require('./classes/User');
 const db = require('./database');
 
 router.post('/', async (req, res) => {
-    const userData = req.body;
-    const user = new User(userData);
-    await db.createUser(user);
-    res.status(201).send(user);
+  const userData = req.body;
+  const user = new User(userData);
+  await db.createUser(user);
+  res.status(201).send(user);
 });
 
 router.get('/:userId', async (req, res) => {
-    const userId = req.params.userId;
-    const user = await db.getUserProfile(userId);
-    res.send(user);
+  const userId = req.params.userId;
+  const user = await db.getUserProfile(userId);
+  res.send(user);
 });
 
 router.put('/:userId', async (req, res) => {
-    const userId = req.params.userId;
-    const updatedData = req.body;
-    await db.updateUserProfile(userId, updatedData);
-    res.send(`User profile with ID: ${userId} updated successfully.`);
+  const userId = req.params.userId;
+  const updatedData = req.body;
+  await db.updateUserProfile(userId, updatedData);
+  res.send(`User profile with ID: ${userId} updated successfully.`);
 });
 
 router.delete('/:userId', async (req, res) => {
-    const userId = req.params.userId;
-    await db.deleteUserProfile(userId);
-    res.send(`User profile with ID: ${userId} deleted successfully.`);
+  const userId = req.params.userId;
+  await db.deleteUserProfile(userId);
+  res.send(`User profile with ID: ${userId} deleted successfully.`);
 });
 
 module.exports = router;
-
-    
