c32d96c7b9d4b3027afef83e691233211f6ac24c
kyaukyuai <y.kakui@gmail.com>
2024-01-14 21:08:17 +0900

    Add files via your-dev-team

--- src/database.js
+++ src/database.js
@@ -1,20 +1,18 @@
-    // This is a placeholder for a real database.
+// This is a placeholder for a real database.
 // In a real application, here would be the actual database operations like create, read, update and delete.
 
 module.exports = {
-    createUser: () => {},
-    getUserProfile: () => {},
-    updateUserProfile: () => {},
-    deleteUserProfile: () => {},
-    createNews: () => {},
-    getAllNews: () => {},
-    getNews: () => {},
-    updateNews: () => {},
-    deleteNews: () => {},
-    createComment: () => {},
-    getComment: () => {},
-    updateComment: () => {},
-    deleteComment: () => {}
+  createUser: () => {},
+  getUserProfile: () => {},
+  updateUserProfile: () => {},
+  deleteUserProfile: () => {},
+  createNews: () => {},
+  getAllNews: () => {},
+  getNews: () => {},
+  updateNews: () => {},
+  deleteNews: () => {},
+  createComment: () => {},
+  getComment: () => {},
+  updateComment: () => {},
+  deleteComment: () => {},
 };
-
-    
